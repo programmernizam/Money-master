8afd89e49fa81cf6f45f30d07229a094511d8844
programmernizam <mdnizam12109@gmail.com>
2022-02-17 02:49:49 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-console.log('work')
+// Input Filed
+function getInput(id){
+    const expensesInput = document.getElementById(id);
+    const expensesCost = parseFloat(expensesInput.value);
+    if(isNaN(expensesCost) || expensesCost < 0){
+        return alert("Please enter valid number of money");
+    }
+    expensesInput.value = '';
+    return expensesCost;
+}
+function getExpenses(){
+    const foodExpenses = getInput('food-expenses');
+    const rentExpenses = getInput('rent-expenses');
+    const clothesExpenses = getInput('clothes-expenses');
+    const totalExpenses = foodExpenses + rentExpenses + clothesExpenses;
+    return totalExpenses;
+}
+document.getElementById('calculate-btn').addEventListener('click', function(){
+// Total Expenses
+   const totalExpenses = getExpenses()
+    document.getElementById('total-expenses').innerText = totalExpenses;
+    // Total Balance
+    const totalIncome = getInput('total-income');
+    if(totalExpenses > totalIncome){
+        return alert("You don't have enough money for your expenses. Please enter low money for expenses!!")
+    }
+    const totalBalance = totalIncome - totalExpenses;
+    document.getElementById('total-balance').innerText = totalBalance;
+})
+document.getElementById('saving-btn').addEventListener('click', function(){
+    const totalIncome = getInput('total-income');
+    const totalBalance = document.getElementById('total-balance');
+    const savingNumber = getInput('saving-number');
+    const savingAmount = totalIncome * (savingNumber / 100);
+    if(savingAmount > totalIncome || savingAmount < 0){
+        return alert("Saving amount bigger then Income")
+    }
+    document.getElementById('saving-amount').innerText = savingAmount;
+    
+})
